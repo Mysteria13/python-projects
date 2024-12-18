0c19d2f3598f187f4677e9be1de4c473e5f05850
Mysteria13 <lisaeseohenohimai@gmail.com>
2024-12-18 17:53:18 -0500

    save

--- studentddb.py
+++ studentddb.py
@@ -1,9 +1,10 @@
-import streamlit as st 
-import pandas as pd
+import streamlit as st #this creates web page for your python app
+import pandas as pd#this works with csv and converts to table
+import plotly.express as px#this is used to plot charts
 st.set_page_config(page_title='Student Databse',page_icon='👌')
 st.title(' Student database App ')
 
-menu = st.sidebar.selectbox("Choose an option",["input scores","scores database"])
+menu = st.sidebar.selectbox("Choose an option",["input scores","scores database/chart"])
 
 try: #attempt to do what is below 
    readcsv = pd.read_csv('scores.csv')
@@ -12,9 +13,13 @@ except:#if there is an error in the above attempt then do this
 
      #csv : comma seperated values
 #menu page to only show the table 
-if menu == "scores database":
+if menu == "scores database/chart":
     st.table(readcsv)
-     
+    subjects = ["Math","English","Science","Art","History","Geography"]
+    subjectstable = readcsv[subjects].mean().reset_index()
+
+    barchart = px.bar(subjectstable,x='index',y=0) 
+    st.plotly_chart(barchart)    
 if menu == "input scores": 
      name = st.text_input('Please enter Student name')
      col1, col2, = st.columns(2)
@@ -47,7 +52,7 @@ if menu == "input scores":
      if st.button ("Save student scores"):
           if name and math and English and Science and Art and History and Geo:
            st.write (name,"your total score is",Total,"average is",ave,"grade is", grade, "Good job!!!")
-          scoresdict = {"Name":[name],"Math":[math],"Science":[Science],"Art":[Art],"History":[History],"Geography":[Geo],"Average":[ave],"Grade":[grade]}
+          scoresdict = {"Name":[name],"Math":[math],"English":[English],"Science":[Science],"Art":[Art],"History":[History],"Geography":[Geo],"Average":[ave],"Grade":[grade]}
           st.write (scoresdict)
           student_table = pd.DataFrame(scoresdict)
           st.table(student_table)
